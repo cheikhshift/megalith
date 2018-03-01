c877cfe7acee66ec132995da0d0f537d6aafb825
Cheikh Seck <jvmstix@gmail.com>
2018-03-01 18:16:09 +0000

    Add files via upload

--- mega_config.go
+++ mega_config.go
@@ -2,39 +2,76 @@ package main
 
 import "time"
 
-var megaWorkspace string = "megaWorkSpace"
+// Megalith directory names
+var (
+	// name of megalith workspace directory.
+	megaWorkspace string = "megaWorkSpace"
+	// name of megalith log directory.
+	logDirectory string = "logDirectory"
+)
 
-var ContentJson = "application/json"
-var TwFormat = "%s%s"
-var DayInSeconds int64 = 86400
-
+// Name of megalith settings file.
 var configName string = "settings.json"
-var logDirectory string = "logDirectory"
 
-var logformat string = "log.%s.json"
-var urlformat string = "%s%s"
-
+// Run megalith in production mode.
 var Production bool = false
 
+// Interval to perform requests
+// to the servers your are
+// monitoring
 const Checkinterval time.Duration = 2 * time.Minute // minutes
 
-//SMTP notifier constants
-const DownMsg string = `Dear %s
+// String formats
+const (
+	//log file name format
+	logformat string = "log.%s.json"
+	// URL format : HOST_NAME + PATH
+	urlformat string = "%s%s"
+	// Twilio Phone format : AREA_CODE + PHONE_NUMBER
+	TwFormat = "%s%s"
+)
+
+//Notifier message constants
+const (
+	DownMsg string = `Dear %s
 Server %s(%s) has an uptime below %.2f%%.
 
 Megalith`
-const DownSub string = "%s is server down."
-const NewLine string = "\n"
-const HeaderSeparator string = ":"
-const SMTPNoSettingsError string = "SMTP settings are not set."
-const EmptyString string = ""
-const SMTPMessage string = "From: %s\nTo: %s\nSubject:%s\nMIME-version: 1.0;\nContent-Type: text/plain; charset=\"UTF-8\";\n\n%s"
-const SMTPAddress string = "%s:%s"
+	DownSub string = "%s is server down."
+)
+
+// Notifier SMTP settings
+const (
+	SMTPNoSettingsError string = "SMTP settings are not set."
+	SMTPMessage         string = "From: %s\nTo: %s\nSubject:%s\nMIME-version: 1.0;\nContent-Type: text/plain; charset=\"UTF-8\";\n\n%s"
+	SMTPAddress         string = "%s:%s"
+)
 
 // HTTP request constants
-const MaxPossibleHTTPSuccessCode int = 300
-const Zero int = 0
-const NetworkAccessErrorCode int = 900
+const (
+	//request considered successful if
+	// status code is below 300.
+	MaxPossibleHTTPSuccessCode int = 300
+	// Network error status code.
+	NetworkAccessErrorCode int = 900
+)
 
 // Timer constants
 const BeatAt string = "Beat at "
+
+// Browser open command
+const (
+	DarwinOpen string = "open http://localhost:9001/index"
+	NTOpen     string = "xdg-open http://localhost:9001/index"
+	LinuxOpen  string = "cmd /C start http://localhost:9001/index"
+)
+
+// Misc constants
+const (
+	NewLine         string = "\n"
+	HeaderSeparator string = ":"
+	EmptyString     string = ""
+	ContentJson     string = "application/json"
+	DayInSeconds    int64  = 86400
+	Zero            int    = 0
+)
